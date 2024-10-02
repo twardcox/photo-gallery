c439b01f343e87e82ef8e98813d9e4467d3b3fc6
travis-cox2 <tcox2@apple.com>
2024-10-02 10:47:49 -0700

    add react photo album

--- app/photos/gallery/page.tsx
+++ app/photos/gallery/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 
-import ImageGallery from "react-image-gallery";
 import { useQuery } from '@tanstack/react-query'
 import fetchImages from "../../api/queries";
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { RowsPhotoAlbum } from "react-photo-album";
 
 export default withPageAuthRequired(function Page() {
   const { data } = useQuery({ queryKey: ['images'], queryFn: fetchImages })
 
   return (
     <main>
-      {data && <ImageGallery items={data} />}
+
+      {data && <RowsPhotoAlbum photos={data} />}
     </main>
   )
 })
