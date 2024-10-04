a7e2a36e1db6bcc5be78b85ed2ac6cd4704023c0
travis-cox2 <tcox2@apple.com>
2024-10-04 12:53:05 -0700

    views

--- components/AlbumOptions.tsx
+++ components/AlbumOptions.tsx
@@ -1,47 +1,95 @@
 'use client'
 
+import { ChangeEvent } from "react";
+
 type AlbumOptionsProps = {
   metadataView: () => void;
-  albumView: (view: 'columns' | 'rows' | 'masonry') => void;
+  handleAlbumView: (view: 'columns' | 'rows' | 'masonry') => void;
+  handleImageView: (view: 'frames' | 'carousel') => void;
+  viewOptions: 'frames' | 'carousel';
+  columns: number;
+  handleColumnsChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+  albumView: 'columns' | 'rows' | 'masonry';
 }
 
 
-const AlbumOptions = ({ metadataView, albumView }: AlbumOptionsProps) => {
-  // a series of icons that represent view options
+const AlbumOptions = ({
+  metadataView,
+  handleAlbumView,
+  handleImageView,
+  viewOptions,
+  columns,
+  handleColumnsChange,
+  albumView
+}: AlbumOptionsProps) => {
+
+  const renderOptions = (num: number) => {
+    return (
+      <option key={num} value={num}>
+        {num === 0 ? 'Auto' : `${num} Column${num > 1 ? 's' : ''}`}
+      </option>
+    );
+  }
 
   return (
-    <div className="album-options">
-      <button title="View Metadata" onClick={metadataView}>
-        <i className="material-icons">
-          description
-        </i>
-      </button>
-      <button title="Carousel View">
-        <i className="material-icons">
-          view_carousel
-        </i>
-      </button>
-      <button title="Pane View">
-        <i className="material-icons">
-          photo_library
-        </i>
-      </button>
-      <button title="Columns View" onClick={() => albumView('columns')}>
-        <i className="material-icons">
-          view_column
-        </i>
-      </button>
-      <button title="Rows View" onClick={() => albumView('rows')}>
-        <i className="material-icons">
-          view_stream
-        </i>
-      </button>
-      <button title="Masonry View" onClick={() => albumView('masonry')}>
-        <i className="material-icons">
-          view_module
-        </i>
-      </button>
-    </div>
+    <>
+      {
+        viewOptions === 'frames' && albumView === 'columns' &&
+        <div className="select-columns">
+          <select
+            value={columns}
+            onChange={handleColumnsChange}
+            title="Select number of columns"
+            className="column-selector"
+          >
+            {[0, 1, 2, 3, 4, 5].map((num) => (
+              renderOptions(num)
+            ))}
+          </select>
+        </div>
+      }
+      <div className="album-options">
+        <button title="View Metadata" onClick={metadataView}>
+          <i className="material-icons">
+            description
+          </i>
+        </button>
+        <button title="Carousel View" onClick={() => handleImageView('carousel')}>
+          <i className="material-icons">
+            view_carousel
+          </i>
+        </button>
+        <button title="Frames View" onClick={() => handleImageView('frames')}>
+          <i className="material-icons">
+            photo_library
+          </i>
+        </button>
+        {
+          viewOptions === 'frames' &&
+          (
+            <>
+              <button title="Columns View" onClick={() => handleAlbumView('columns')}>
+                <i className="material-icons">
+                  view_column
+                </i>
+              </button>
+              <button title="Rows View" onClick={() => handleAlbumView('rows')}>
+                <i className="material-icons">
+                  view_stream
+                </i>
+              </button>
+              <button title="Masonry View" onClick={() => handleAlbumView('masonry')}>
+                <i className="material-icons">
+                  view_module
+                </i>
+              </button>
+
+
+            </>
+          )
+        }
+      </div>
+    </>
   );
 }
 
--- components/PhotoAlbum.tsx
+++ components/PhotoAlbum.tsx
@@ -2,104 +2,100 @@
 
 import { PhotoAlbumProps, RenderImageContextProps } from "@/types/photoTypes";
 import AlbumOptions from "./AlbumOptions";
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import PhotoAlbum, { RenderImageProps } from "react-photo-album";
 import "react-photo-album/styles.css";
 import Image from "next/image";
+import classnames from "classnames";
 
 const Album: FC<PhotoAlbumProps> = ({ photos }) => {
-  const [viewOptions, setViewOptions] = useState<'pane' | 'carousel'>('pane');
+  const [viewOptions, setViewOptions] = useState<'frames' | 'carousel'>('frames');
   const [albumView, setAlbumView] = useState<'columns' | 'rows' | 'masonry'>('rows');
   const [viewMetadata, setViewMetadata] = useState(false);
+  const [columns, setColumns] = useState<number>(0);
 
-  const renderMetadata = (metadata: any) => {
-    return Object.keys(metadata).map((key) => {
-      return <p key={key}>{key}: {metadata[key]}</p>
-    })
-  }
-
-  function renderNextImage(
-    { alt = "", title, sizes }: RenderImageProps,
-    { photo, width, height }: RenderImageContextProps,
-  ) {
-    console.log('photo: ', photo);
-    return (
-      <div
-        style={{
-          width: "100%",
-          position: "relative",
-          aspectRatio: `${width} / ${height}`,
-        }}
-      >
-        <Image
-          className="photo"
-          fill
-          src={photo}
-          alt={alt}
-          title={title}
-          sizes={sizes}
-          placeholder={"blurDataURL" in photo ? "blur" : undefined}
-        />
-        {
-          viewMetadata &&
-          <div className="photo-data">
-            {photo.metadata && renderMetadata(photo.metadata)}
-          </div>
-        }
-      </div>
-    );
-  }
 
   const handleMetadataView = () => {
     setViewMetadata(!viewMetadata);
   }
 
   const handleAlbumView = (view: 'columns' | 'rows' | 'masonry') => {
-
     setAlbumView(view);
   }
 
+  const handleImageView = (view: 'frames' | 'carousel') => {
+    setViewOptions(view);
+  }
+
+  const handleColumnsChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const newColumns = parseInt(event.target.value, 10);
+    setColumns(newColumns);
+  };
+
+  const renderMetadata = (metadata: any) => {
+    return Object.keys(metadata).map((key) => {
+      return <p className="photo-data" key={key}>{key}: {metadata[key]}</p>
+    })
+  }
+
+  const renderNextImage = (
+    { alt = "", title, sizes }: RenderImageProps,
+    { photo, width, height }: RenderImageContextProps,
+  ) => {
+    return (
+      <div className="photo">
+        <div
+          style={{
+            width: "100%",
+            position: "relative",
+            aspectRatio: `${width} / ${height}`,
+          }}
+        >
+          <Image
+
+            fill
+            src={photo}
+            alt={alt}
+            title={title}
+            sizes={sizes}
+            placeholder={"blurDataURL" in photo ? "blur" : undefined}
+          />
+        </div>
+        {
+          viewMetadata &&
+          photo.metadata && renderMetadata(photo.metadata)
+        }
+      </div>
+    );
+  }
 
   return (
-    <div className="album-wrapper">
-      <div className="options">
-        <AlbumOptions metadataView={handleMetadataView} albumView={handleAlbumView} />
+    <>
+      <div className={classnames("options", { hasColumns: viewOptions === 'frames' && albumView === 'columns' })}>
+        <AlbumOptions
+          metadataView={handleMetadataView}
+          handleAlbumView={handleAlbumView}
+          handleImageView={handleImageView}
+          viewOptions={viewOptions}
+          columns={columns}
+          handleColumnsChange={handleColumnsChange}
+          albumView={albumView}
+        />
       </div>
-      {/* <div className="album">
-        {photos.map(photo => {
-
-          const divStyle = {
-            height: `${photo.height}px`,
-            // width: `${photo.width}px`,
-          };
-          return (
-            <div key={photo.key} style={divStyle}>
-              <Image
-                className="photo"
-                key={photo.key}
-                src={photo.src || '/default-image.jpg'}
-                alt={photo.key}
-                style={{ objectFit: "contain" }}
-                loading="lazy"
-                height={photo.height}
-                width={photo.width}
-              />
-              {
-                viewMetadata &&
-                <div className="photo-data">
-                  {photo.metadata && renderMetadata(photo.metadata)}
-                </div>
-              }
-            </div>)
-        })}
-      </div> */}
       <PhotoAlbum
         layout={albumView}
         photos={photos}
         render={{ image: renderNextImage }}
         padding={2}
+        columns={(containerWidth) => {
+          if (columns > 0) return columns;
+          if (containerWidth < 400) return 1;
+          if (containerWidth < 600) return 2;
+          if (containerWidth < 900) return 3;
+          return 4;
+        }}
       />
-    </div>
+    </>
   );
 }
 
